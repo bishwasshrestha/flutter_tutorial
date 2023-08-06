12c7f910be3bdaaecc7413f08951d6a768d48f77
Bishwas Shrestha <bishwas.shrestha@gmail.com>
2023-08-06 13:45:52 +0300

    world time app prints current time and date on the device

--- worldtime/lib/main.dart
+++ worldtime/lib/main.dart
@@ -3,8 +3,7 @@ import 'package:worldtime/pages/choose_location.dart';
 import 'package:worldtime/pages/home.dart';
 import 'package:worldtime/pages/loading.dart';
 
-void main() {
-  runApp(MaterialApp(
+void main() => runApp(MaterialApp(
       // home: const Home(),
       initialRoute: '/', //overriding the map to display on load
       routes: {
@@ -12,4 +11,4 @@ void main() {
         '/home': (context) => const Home(),
         '/location': (context) => const ChooseLocation()
       }));
-}
+
